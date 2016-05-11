a19ed7f68ffa9a30358f09f39107fee09d1edebd
Daniel Freitas <daniel.exatas@gmail.com>
2016-05-11 11:25:50 -0300

    Add files via upload

---
+++ src/urijudge/iniciante/SomaSimples.java
@@ -0,0 +1,19 @@
+package urijudge.iniciante;
+
+import java.util.Scanner;
+
+public class SomaSimples {
+
+	public static void main(String[] args) {
+		int A, B, SOMA;
+		Scanner read = new Scanner(System.in);
+		
+		A = read.nextInt();
+		B = read.nextInt();
+		
+		SOMA = A + B;
+		
+		System.out.println("SOMA = "+SOMA);
+	}
+
+}
