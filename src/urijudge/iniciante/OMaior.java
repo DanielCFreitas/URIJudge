74b688054dd4a6896e286607a08a1aff53fbeb27
Daniel Freitas <daniel.exatas@gmail.com>
2016-05-14 17:18:49 -0300

    Add files via upload

---
+++ src/urijudge/iniciante/OMaior.java
@@ -0,0 +1,23 @@
+package urijudge.iniciante;
+
+import java.util.Scanner;
+
+public class OMaior {
+
+	public static void main(String[] args) {
+		int num1, num2, num3, maior;
+		Scanner read = new Scanner(System.in);
+
+		num1 = read.nextInt();
+		num2 = read.nextInt();
+		num3 = read.nextInt();
+
+		read.close();
+
+		maior = (num1 + num2 + Math.abs(num1 - num2)) / 2;
+		maior = (num3 + maior + Math.abs(num3 - maior)) / 2;
+
+		System.out.printf("%d eh o maior\n", maior);
+	}
+
+}
