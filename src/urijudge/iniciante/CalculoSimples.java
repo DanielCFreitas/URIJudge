75e829e8fd2f8caf04be87a5a7f17467fe62050c
Daniel Freitas <daniel.exatas@gmail.com>
2016-05-14 16:54:43 -0300

    Add files via upload

---
+++ src/urijudge/iniciante/CalculoSimples.java
@@ -0,0 +1,25 @@
+package urijudge.iniciante;
+
+import java.util.Scanner;
+
+public class CalculoSimples {
+
+	public static void main(String[] args) {
+		int cod_peca1, cod_peca2, quantidade_peca1, quantidade_peca2;
+		double preco_peca1, preco_peca2;
+		Scanner read = new Scanner(System.in);
+
+		cod_peca1 = read.nextInt();
+		quantidade_peca1 = read.nextInt();
+		preco_peca1 = read.nextDouble();
+
+		cod_peca2 = read.nextInt();
+		quantidade_peca2 = read.nextInt();
+		preco_peca2 = read.nextDouble();
+
+		read.close();
+
+		System.out.printf("VALOR A PAGAR: R$ %.2f\n", preco_peca1 * quantidade_peca1 + preco_peca2 * quantidade_peca2);
+	}
+
+}
