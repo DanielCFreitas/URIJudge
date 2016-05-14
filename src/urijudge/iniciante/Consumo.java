685414fd98ebcc99196ee2142621930730efbd12
Daniel Freitas <daniel.exatas@gmail.com>
2016-05-14 18:20:20 -0300

    Add files via upload

---
+++ src/urijudge/iniciante/Consumo.java
@@ -0,0 +1,20 @@
+package urijudge.iniciante;
+
+import java.util.Scanner;
+
+public class Consumo {
+
+	public static void main(String[] args) {
+		int x;
+		double y;
+		Scanner read = new Scanner(System.in);
+
+		x = read.nextInt();
+		y = read.nextDouble();
+
+		read.close();
+
+		System.out.printf("%.3f km/l\n", x / y);
+
+	}
+}
