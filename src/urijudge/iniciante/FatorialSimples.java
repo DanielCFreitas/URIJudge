020f744fc3e61bcd44e90b0300b4198dca8b6324
Daniel Freitas <daniel.exatas@gmail.com>
2016-06-16 10:36:32 -0300

    Add files via upload

---
+++ src/urijudge/iniciante/FatorialSimples.java
@@ -0,0 +1,27 @@
+package urijudge.iniciante;
+
+import java.util.Scanner;
+
+public class FatorialSimples {
+
+	public static void main(String[] args) {
+		int N, fatorial = 1, cont = 1;
+		Scanner read = new Scanner(System.in);
+
+		N = read.nextInt();
+
+		if (N == 0) {
+			System.out.printf("%d\n", 0);
+		} else {
+			while (cont <= N) {
+				fatorial *= cont;
+				cont++;
+			}
+			System.out.printf("%d\n", fatorial);
+		}
+
+		read.close();
+
+	}
+
+}
